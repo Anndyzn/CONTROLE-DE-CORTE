b21660c6712ac1e602e5be80c2a938f97b299fdc
Andy Higa <andyhiga123@gmail.com>
2026-08-12 11:59:13 -0300

    chore: prepara aplicacao para deploy no Render

--- src/server.ts
+++ src/server.ts
@@ -20,8 +20,8 @@ app.use(producaoRoutes)
 app.use(itensRoutes)
 app.use(consultasRoutes)
 
-const PORT = Number(process.env.PORT) || 3000
+const PORT = process.env.PORT || 3000
 
-app.listen(PORT, "0.0.0.0", () => {
+app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`)
 })
